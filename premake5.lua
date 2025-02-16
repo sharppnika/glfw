219df68bd5ff0bf126542c5f83d142bbd1d092d6
sharppnika <1730413710@qq.com>
2025-02-16 23:20:42 +0800

    2.16

--- premake5.lua
+++ premake5.lua
@@ -1,27 +1,68 @@
 project "GLFW"
-	kind "StaticLib"
-	language "C"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
-	}
-	filter "system:linux"
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/glfw_config.h",       --？？should it to be delete?
+        "src/internal.h",       --
+        "src/platform.h",       --
+        "src/mappings.h",       --
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/platform.c",           --
+        "src/vulkan.c",
+        "src/window.c",
+        "src/egl_context.c",        --
+        "src/osmesa_context.c",     --
+        "src/null_platform.h",      --
+        "src/null_joystick.h",      --
+        "src/null_init.c",          --
+    
+        "src/null_monitor.c",       --
+        "src/null_window.c",        --
+        "src/null_joystick.c",      --
+    }
+
+	filter "system:windows"
+        systemversion "latest"
+        staticruntime "on"
+
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_module.c",   --
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.h",     --
+            "src/win32_time.c",
+            "src/win32_thread.h",   --解决Vulkan.obj相关问题
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
+
+        defines 
+        { 
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
+
+    filter "system:linux"
 		pic "On"
 
 		systemversion "latest"
-		staticruntime "On"
+		staticruntime "on"
 
 		files
 		{
@@ -42,33 +83,11 @@ project "GLFW"
 			"_GLFW_X11"
 		}
 
-	filter "system:windows"
-		systemversion "latest"
-		staticruntime "On"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
-
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+        
